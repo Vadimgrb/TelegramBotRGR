b9ce5b841703bb8b9667b499959392a3cd7fbb55
Vadimgrb <137248328+Vadimgrb@users.noreply.github.com>
2024-10-10 22:08:08 +1000

    Data.py

--- Data.py
+++
@@ -1,9 +0,0 @@
-movies_data = [
-    {"title": "Фильм 1", "genre": "Drama", "year": 2020, "revenue": 1000000, "awards": ["Oscar"]},
-    {"title": "Фильм 2", "genre": "Comedy", "year": 2021, "revenue": 500000, "awards": []},
-    {"title": "Фильм 3", "genre": "Action", "year": 2022, "revenue": 2000000, "awards": ["Golden Globe"]},
-    {"title": "Фильм 4", "genre": "Horror", "year": 2023, "revenue": 300000, "awards": ["Oscar", "Golden Globe"]},
-    {"title": "Фильм 5", "genre": "Drama", "year": 2019, "revenue": 1500000, "awards": []},
-]
-
-
